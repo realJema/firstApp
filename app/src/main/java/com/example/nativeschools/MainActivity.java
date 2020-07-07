102f7fa5367f431b569ceae2c71125efa5658256
realJema <realJema@gmail.com>
2020-07-07 13:18:37 +0100

    First app

---
+++ app/src/main/java/com/example/nativeschools/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nativeschools;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CardView card_view = (CardView) findViewById(R.id.home_btn); // creating a CardView and assigning a value.
+
+        card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do whatever you want to do on click (to launch any fragment or activity you need to put intent here.)
+                setContentView(R.layout.home);
+            }
+        });
+    }
+}
